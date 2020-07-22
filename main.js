fbc7109a1b306698bde80cedbefc9553948c974f
tsmsp <tsm_sp@hotmail.com>
2020-07-22 17:56:28 -0300

    custom app

--- main.js
+++ main.js
@@ -1,43 +1,64 @@
 // Modules to control application life and create native browser window
-const {app, BrowserWindow} = require('electron')
+const {app, BrowserWindow, Tray, Menu} = require('electron')
 const path = require('path')
+const assetsDirectory = path.join(__dirname, 'assets')
 
-function createWindow () {
-  // Create the browser window.
-  const mainWindow = new BrowserWindow({
-    width: 800,
-    height: 600,
-    webPreferences: {
-      preload: path.join(__dirname, 'preload.js')
-    }
-  })
+let tray = undefined
 
-  // and load the index.html of the app.
-  mainWindow.loadFile('index.html')
-
-  // Open the DevTools.
-  // mainWindow.webContents.openDevTools()
+const createTray = () => {
+  tray = new Tray(path.join(assetsDirectory, 'iconTemplate.png'))    
+  
+   const contextMenu = Menu.buildFromTemplate([    
+    { label: 'Music Player', type: 'radio', checked: false }    
+   ])
+  tray.setToolTip('Player Study')
+  tray.setContextMenu(contextMenu)
 }
 
-// This method will be called when Electron has finished
-// initialization and is ready to create browser windows.
-// Some APIs can only be used after this event occurs.
+function createWindow () {  
+  const win = new BrowserWindow({      
+    icon: path.join(__dirname, 'assets/icons/64x64.png'),  
+    backgroundColor: '#312450',    
+    width: 1366,
+    height: 762,            
+    titleBarStyle: 'show',
+    fullscreen: false,    
+    show: true,    
+    webPreferences: {
+      nodeIntegration: true,
+      images: true,
+      sandbox: true,
+      preload: path.join(app.getAppPath(), 'preload.js')      
+    }    
+  })
+
+//app.dock.hide()
+app.on('ready', () => {
+  createTray();
+  createWindow();
+  win.once('ready-to-show', () => {
+    win.show();
+  }) 
+})
+
+  // and load the index.html of the app  
+  win.loadURL('https://gk4m.github.io/vue-spotify/#/browse/genres')  
+
+  // Open the DevTools.
+  // win.webContents.openDevTools()
+}
+
 app.whenReady().then(() => {
   createWindow()
   
   app.on('activate', function () {
-    // On macOS it's common to re-create a window in the app when the
-    // dock icon is clicked and there are no other windows open.
     if (BrowserWindow.getAllWindows().length === 0) createWindow()
   })
 })
 
-// Quit when all windows are closed, except on macOS. There, it's common
-// for applications and their menu bar to stay active until the user quits
-// explicitly with Cmd + Q.
+
 app.on('window-all-closed', function () {
   if (process.platform !== 'darwin') app.quit()
 })
 
-// In this file you can include the rest of your app's specific main process
-// code. You can also put them in separate files and require them here.
+
